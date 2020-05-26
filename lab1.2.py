4cb954f9a9aa5fcc9a483dea236c480c75c4fd10
Artem_Vetoshev <tema.vetoshev@gmail.com>
2020-05-27 00:36:11 +0300

    Added .py files for lab1 (NIR)

---
+++ lab1.2.py
@@ -0,0 +1,37 @@
+import random
+import matplotlib.pyplot as plt
+
+M = 500
+N = 11
+p = 0.005
+probability_l = []
+avg_T = []
+
+
+def send_messages(pp):
+    i = 0
+    messages_sum = 0
+    while i <= N:
+        if random.choices([1, 0], [pp, 1 - pp])[0] == 1:
+            messages_sum += 1
+        i += 1
+    return messages_sum
+
+
+while p <= (1/N):
+    sum_of_sum_messages = []
+    collision = 0
+    _sum = 0
+    probability_l.append(p)
+    for f_slot in range(M):
+        messages_sum = send_messages(p)
+        # если отправили два сообщения одновременно
+        if messages_sum > 1:
+            # случится коллизия
+            collision += messages_sum
+        _sum += messages_sum
+    avg_T.append(collision / _sum)
+    p += 0.01
+
+plt.plot(probability_l, avg_T)
+plt.show()
---
+++ lab1.py
@@ -0,0 +1,42 @@
+import random
+import matplotlib.pyplot as plt
+
+M = 10000
+N = 11
+p = 0
+probability_l = []
+avg_T = []
+
+
+def send_messages(pp):
+    i = 0
+    messages_sum = 0
+    while i <= N:
+        if random.choices([1, 0], [pp, 1 - pp])[0] == 1:
+            messages_sum += 1
+        i += 1
+    return messages_sum
+
+
+while p <= (1/N):
+    sum_of_sum_messages = []
+    awaiting = []
+    probability_l.append(p)
+    for f_slot in range(M):
+        messages_sum = send_messages(p)
+        if not awaiting:
+            awaiting.append(0)
+        elif awaiting[f_slot - 1] <= 0:
+            awaiting.append(messages_sum)
+        else:
+            awaiting.append((awaiting[f_slot - 1] - 1 + messages_sum))
+    awaiting_sum = sum(awaiting)
+    if not avg_T:
+        avg_T.append(0)
+    else:
+        avg_T.append(awaiting_sum / p)
+    p += 0.01
+
+
+plt.plot(probability_l, avg_T)
+plt.show()
